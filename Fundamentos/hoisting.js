1c2d84bb7169029ee924601a344fe7fd27ce31d9
andreivercosa <andreivercosa@gmail.com>
2019-02-08 18:19:14 -0400

    hoisting

---
+++ Fundamentos/hoisting.js
@@ -0,0 +1,7 @@
+console.log('a = ', a)//ele move a declaração apra o topo no caso das variaveis var
+var a = 2
+console.log('a = ', a)
+
+//console.log('a = ', a) // isso não ocorre nas variaveis let
+let a = 2
+console.log('a = ', a)
