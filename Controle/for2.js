55c754b9a1496b91c6aa9250176af73afe4bb2dc
andreivercosa <andreivercosa@gmail.com>
2019-03-19 15:14:40 -0400

    fim_contole

--- Controle/for2.js
+++ Controle/for2.js
@@ -1,6 +1,6 @@
 const notas = [6.7, 7.4, 9.8, 8.1, 7.7]
 
-for(i in notas){
+for(let i in notas){
     console.log(i, notas[i])
 }
 
@@ -11,6 +11,7 @@ const pessoa = {
     peso: 64
 }
 
-for(atributos in pessoa){
+for(let atributos in pessoa){
     console.log(`${atributos} = ${pessoa[atributos]}`)
 }
+
